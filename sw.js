d0a2d2cf5286c6591f7027df1a8e917da7501bd3
nickboy20-sudo <nickboy20@gmail.com>
2026-07-30 14:32:20 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='vremea-v1';
-self.addEventListener('install', e=>{self.skipWaiting()});
-self.addEventListener('activate', e=>{self.clients.claim()});
-self.addEventListener('fetch', e=>{});
